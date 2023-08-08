b9869e5b3d08722defee5229725467c338d86dea
Muhammad Hamza <140630940+hc-huxai@users.noreply.github.com>
2023-08-08 01:51:33 +0000

    init

---
+++ 01-shadcn-form/src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { ThemeToggle } from '@/components/ThemeToggle';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <NextThemesProvider attribute='class' defaultTheme='system' enableSystem>
+      <Component {...pageProps} />
+      <ThemeToggle className='absolute top-6 right-6 '/>
+    </NextThemesProvider>
+  );
+}
