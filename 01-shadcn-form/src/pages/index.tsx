d4982382d2450061441277a0a6119f508c14a5ac
Muhammad Hamza <140630940+hc-huxai@users.noreply.github.com>
2023-08-08 17:21:22 +0000

    modified:   01-shadcn-form/package-lock.json 	modified:   01-shadcn-form/package.json 	new file:   01-shadcn-form/src/components/ui/card.tsx 	new file:   01-shadcn-form/src/components/ui/form.tsx 	new file:   01-shadcn-form/src/components/ui/input.tsx 	new file:   01-shadcn-form/src/components/ui/label.tsx 	new file:   01-shadcn-form/src/components/ui/select.tsx 	modified:   01-shadcn-form/src/pages/_app.tsx 	modified:   01-shadcn-form/src/pages/index.tsx 	new file:   01-shadcn-form/src/validators/auth.ts

--- 01-shadcn-form/src/pages/index.tsx
+++ 01-shadcn-form/src/pages/index.tsx
@@ -1,10 +1,154 @@
-import { Inter } from 'next/font/google'
-import { Button } from '@/components/ui/button'
+import { Inter } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useForm } from "react-hook-form";
+import { registerSchema } from "@/validators/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { log } from "console";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+type Input = z.infer<typeof registerSchema>;
 
 export default function Home() {
+  const form = useForm<Input>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      email: "",
+      name: "",
+      studentId: "",
+      year: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  function onSubmit(data: Input) {
+    console.log(data);
+  }
+
   return (
-    <Button>Hello World</Button>
-  )
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Register</CardTitle>
+          <CardDescription>Start the journey with us today.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your email..." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Full Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your name..." {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="studentId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Student ID</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your student id..."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="year"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Year of study</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a verified email to display" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {[10, 11, 12, 13].map((year) => {
+                          return (
+                            <SelectItem value={year.toString()} key={year}>
+                              Year {year}
+                            </SelectItem>
+                          );
+                        })}
+                      </SelectContent>
+                    </Select>
+                    {/* <FormDescription>
+                      You can manage email addresses in your{" "}
+                      <Link href="/examples/forms">email settings</Link>.
+                    </FormDescription> */}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit" variant="default">
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
---
+++ 01-shadcn-form/src/validators/auth.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  email: z.string().email(),
+  name: z.string().min(3).max(255),
+  studentId: z.string().min(7).max(7),
+  year: z.string().min(2).max(10),
+  password: z.string().min(6).max(100),
+  confirmPassword: z.string().min(6).max(100),
+});
